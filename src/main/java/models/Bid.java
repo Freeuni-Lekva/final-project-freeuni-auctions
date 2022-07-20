3a37eec66fb2443ea138a76837d335f13f1c8e8d
Lizi <lekse20@freeuni.edu.ge>
2022-07-20 19:07:27 +0400

    add bid and category classes

---
+++ src/main/java/models/Bid.java
@@ -0,0 +1,39 @@
+package models;
+
+import java.math.BigDecimal;
+import java.sql.Date;
+import java.util.UUID;
+
+public class Bid {
+    private final UUID id, bidder_id, item_id;
+    private final BigDecimal amount;
+    private final Date time;
+
+    public Bid(UUID id, UUID bidder_id, UUID item_id, BigDecimal amount, Date time) {
+        this.id = id;
+        this.bidder_id = bidder_id;
+        this.item_id = item_id;
+        this.amount = amount;
+        this.time = time;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public UUID getBidder_id() {
+        return bidder_id;
+    }
+
+    public UUID getItem_id() {
+        return item_id;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public Date getTime() {
+        return time;
+    }
+}
