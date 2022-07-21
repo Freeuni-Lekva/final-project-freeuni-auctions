4c85eb6a4f6a27b41a708154eaa8a7067901d701
Lizi <lekse20@freeuni.edu.ge>
2022-07-21 08:39:18 +0400

    add methods to bid dao

--- src/main/java/dao/BidDAO.java
+++ src/main/java/dao/BidDAO.java
@@ -1,8 +1,10 @@
 package dao;
-import models.Bid;
+import models.*;
 
 import java.math.BigDecimal;
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 public class BidDAO {
@@ -15,13 +17,36 @@ public class BidDAO {
 
     public Bid getFromID(UUID bid_id) throws SQLException {
         ResultSet rs = conn.createStatement().executeQuery("SELECT * FROM " + TABLE_NAME + " WHERE id = " + bid_id);
-        rs.next();
+        if (rs.next()) {
+            return createBidFromRow(rs);
+        } else return null;
+    }
+
+    public List<Bid> getAllForUser(UUID user_id) throws SQLException {
+        ResultSet rs = conn.createStatement().executeQuery("SELECT * FROM " + TABLE_NAME + " WHERE bidder_id = " + user_id);
+        List<Bid> result = new ArrayList<Bid>();
+        while (rs.next()) {
+            result.add(createBidFromRow(rs));
+        }
+        return result;
+    }
+
+    public List<Bid> getAllForProduct(UUID product_id) throws SQLException {
+        ResultSet rs = conn.createStatement().executeQuery("SELECT * FROM " + TABLE_NAME + " WHERE product_id = " + product_id);
+        List<Bid> result = new ArrayList<Bid>();
+        while (rs.next()) {
+            result.add(createBidFromRow(rs));
+        }
+        return result;
+    }
+
+    private Bid createBidFromRow(ResultSet rs) throws SQLException {
+        UUID id = UUID.fromString(rs.getString("id"));
         UUID bidder_id = UUID.fromString(rs.getString("bidder_id"));
         UUID product_id = UUID.fromString(rs.getString("product_id"));
         BigDecimal amount = rs.getBigDecimal("amount");
         Date time = rs.getDate("date");
-        return new Bid(bid_id, bidder_id, product_id, amount, time);
+        return new Bid(id, bidder_id, product_id, amount, time);
     }
-
 
 }
