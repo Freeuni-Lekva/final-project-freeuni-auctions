1a93697634bdd1722b7bf2499229fa8a323600f7
Nikoloz Chapidze <nchap20@freeuni.edu.ge>
2022-09-06 19:56:19 +0400

    added sale model

--- src/main/java/models/Sale.java
+++ src/main/java/models/Sale.java
@@ -1,1 +1,23 @@
 package models;
+
+public class Sale {
+    private final long id, product_id, user_id;
+
+    public Sale(long id, long product_id, long user_id) {
+        this.id = id;
+        this.product_id = product_id;
+        this.user_id = user_id;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public long getProduct_id() {
+        return product_id;
+    }
+
+    public long getUser_id() {
+        return user_id;
+    }
+}
