92fc6d3827afa55653969a3ef02c4e494e132f89
sandrokur <91629248+sandrokur@users.noreply.github.com>
2022-09-08 17:59:23 +0400

    category test added

--- src/main/java/Tests/ProductsTest.java
+++ src/main/java/Tests/ProductsTest.java
@@ -7,6 +7,7 @@ import models.Product;
 import models.Status;
 
 import java.util.Date;
+import java.util.List;
 
 public class ProductsTest extends TestCase {
     private ProductDAO productDAO;
@@ -29,18 +30,23 @@ public class ProductsTest extends TestCase {
         Product p = productDAO.getFromID(5);
         assertEquals(p.getName(), "foo");
         assertEquals(p.getAccountId(), 1);
-        assertEquals(p.getDescription(), null);
+        assertNull(p.getDescription());
         assertEquals(p.getCategoryId(), 1);
         assertEquals(p.getBidId(), -1);
         assertEquals(p.getCurrPrice(), 20);
         assertEquals(p.getStatus(), Status.available);
-        assertEquals(p.getImage(), null);
+        assertNull(p.getImage());
     }
 
     public void testGetByCategory() {
         Product pr1 = new Product(1, 1, "foo", 20, new Date());
         Product pr2 = new Product(2, 1, "foo bar", 20, new Date());
         Product pr3 = new Product(3, 2, "bar", 20, new Date());
-
+        productDAO.addProduct(pr1);
+        productDAO.addProduct(pr2);
+        productDAO.addProduct(pr3);
+        List<Product> res = productDAO.getProductsByCategory(1);
+        assertEquals(5, res.get(0).getId());    // 5 and 6 as IDs are IDs on my sql server.
+        assertEquals(6, res.get(1).getId());
     }
 }
