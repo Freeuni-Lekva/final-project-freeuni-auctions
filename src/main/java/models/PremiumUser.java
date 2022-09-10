6b03ab2e1e18ef1b4023f5fe7a7261e69e039183
Lizi <lekse20@freeuni.edu.ge>
2022-09-10 13:38:19 +0400

    change ctor in premium user

--- src/main/java/models/PremiumUser.java
+++ src/main/java/models/PremiumUser.java
@@ -5,10 +5,10 @@ import java.security.NoSuchAlgorithmException;
 public class PremiumUser extends RegularUser {
 
     public PremiumUser(long id, String username, String password, String email, String image, long balance) throws NoSuchAlgorithmException {
-        super(id, username, password, email, image, balance);
+        super(id, username, password, email, image, balance, false);
     }
 
-    public PremiumUser(long id, String username, String password, String email, String image, long balance, boolean isSuspended) {
+    public PremiumUser(long id, String username, String password, String email, String image, long balance, boolean isSuspended) throws NoSuchAlgorithmException {
         super(id, username, password, email, image, balance, isSuspended);
     }
 
